a5c55908cb2020be63b9a6907301569d2ac9c2bd
codeslug <crazyshannon@gmail.com>
2014-03-11 02:26:24 -0700

    first commit

---
+++ main.rb
@@ -0,0 +1,24 @@
+require 'rubygems'
+require 'sinatra'
+
+get '/' do
+  @title = "Home"
+  erb :home
+end
+
+get '/frank' do
+  @title = "My Way"
+  erb :frank
+end
+
+post '/' do
+	@title = "You have submitted a form! (inside main.rb)"
+	@reversed_text = params[:phrase].reverse
+	erb :reverse
+end
+
+get '/:phrase' do
+	@title = "Here's your Reversed Text:"
+	@reversed_text = params[:phrase].reverse
+	erb :reverse
+end
